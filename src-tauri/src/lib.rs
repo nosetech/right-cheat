2ffe6226132e9c21bf70b105d594811f2888bc29
Hiroyuki Nose <80944036+nosetech@users.noreply.github.com>
2025-04-02 11:33:16 +0900

    APIからデータを取得してチートシートを表示する (#3)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,9 +2,6 @@ mod api;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let cheatsheets = api::cheat::CHEATSHEETS.lock().unwrap();
-    println!("読み込んだデータ: {:?}", cheatsheets);
-
     tauri::Builder::default()
         .setup(|app| {
             if cfg!(debug_assertions) {
@@ -46,7 +43,10 @@ pub fn run() {
             }
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![api::cheat::greet])
+        .invoke_handler(tauri::generate_handler![
+            api::cheat::get_cheat_titles,
+            api::cheat::get_cheat_sheet
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/organisms/CheatSheet.tsx
+++ src/components/organisms/CheatSheet.tsx
@@ -1,29 +1,79 @@
 'use client'
 import { CommandField } from '@/components/molecules/CommandField'
-import { Typography } from '@mui/material'
+import {
+  CheatSheetData,
+  CheatSheetTitleData,
+  CommandData,
+} from '@/types/api/CheatSheet'
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from '@mui/material'
 import { Stack } from '@mui/system'
+import { invoke } from '@tauri-apps/api/core'
+import { useEffect, useState } from 'react'
 
 export type CheatSheetProps = {}
 
 export const CheatSheet = (props: CheatSheetProps) => {
+  const [cheatSheetTitles, setCheatSheetTitles] = useState<
+    CheatSheetTitleData | undefined
+  >()
+  const [selectCheatSheet, setCheatSheet] = useState<string>('')
+
+  const [cheatSheetData, setCheatSheetData] = useState<CheatSheetData>()
+
+  useEffect(() => {
+    invoke<string>('get_cheat_titles').then((response) => {
+      const titles: CheatSheetTitleData = JSON.parse(response)
+      setCheatSheetTitles(titles)
+      setCheatSheet(titles.title.length > 0 ? titles.title[0] : '')
+    })
+  }, [])
+
+  useEffect(() => {
+    selectCheatSheet != '' &&
+      invoke<string>('get_cheat_sheet', { title: selectCheatSheet }).then(
+        (response) => {
+          const data: CheatSheetData = JSON.parse(response)
+          setCheatSheetData(data)
+        },
+      )
+  }, [selectCheatSheet])
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setCheatSheet(event.target.value as string)
+  }
+
   return (
-    <Stack padding={1} spacing={1}>
-      <Typography variant='h2'>Terraform CheatSheet</Typography>
+    <Stack padding={1}>
+      <FormControl fullWidth>
+        <InputLabel id='demo-simple-select-label'>CheatSheet</InputLabel>
+        <Select
+          labelId='demo-simple-select-label'
+          id='demo-simple-select'
+          value={selectCheatSheet}
+          label='CheatSheet'
+          onChange={handleChange}
+        >
+          {cheatSheetTitles?.title.map((item, index) => (
+            <MenuItem key={index} value={item}>
+              {item}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       <Stack padding={1} spacing={1} width='400px'>
-        <CommandField description='planの実行' command='terraforom plan' />
-        <CommandField description='planの適用' command='terraform apply' />
-        <CommandField
-          description='planの適用(target指定)'
-          command='terraform apply -target"リソース名"'
-        />
-        <CommandField
-          description='refresh(実環境の内容をTFファイルに反映)の実行'
-          command='terraform apply -refresh-only'
-        />
-        <CommandField
-          description='フォーマットの実行'
-          command='terraform fmt -recursive'
-        />
+        {cheatSheetData?.commandlist.map((item: CommandData, index) => (
+          <CommandField
+            key={index}
+            description={item.description}
+            command={item.command}
+          />
+        ))}
       </Stack>
     </Stack>
   )
